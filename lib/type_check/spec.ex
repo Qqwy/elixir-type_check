2cea528d22233dbabd8fb546e902f11eabafa0e8
Marten/Qqwy <w-m@wmcode.nl>
2020-06-22 20:45:47 +0200

    extracting specs

--- lib/type_check/spec.ex
+++ lib/type_check/spec.ex
@@ -8,32 +8,33 @@ defmodule TypeCheck.Spec do
 
 
   defmacro type(args) do
-    IO.inspect(args)
-    {name, raw_type} = extract_type_name(args)
-    quote do
-      Module.put_attribute(__MODULE__, TypeCheck.Spec.Raw, {unquote(name), unquote(Macro.escape(raw_type))})
-    end
+    build_typedef_ast(args, :type)
   end
 
   defmacro typep(args) do
-    IO.inspect(args)
-    {name, raw_type} = extract_type_name(args)
-    quote do
-      Module.put_attribute(__MODULE__, TypeCheck.Spec.Raw, {unquote(name), unquote(Macro.escape(raw_type))})
-    end
+    build_typedef_ast(args, :typep)
   end
 
   defmacro opaque(args) do
-    IO.inspect(args)
-    quote do
-      44
-    end
+    build_typedef_ast(args, :opaque)
   end
 
   defmacro spec(args) do
-    IO.inspect(args)
+    build_spec_ast(args)
+  end
+
+  # Shared between type, typep and opaque
+  defp build_typedef_ast(args, call_kind) do
+    {name, raw_type} = extract_type_name(args)
     quote do
-      45
+      Module.put_attribute(__MODULE__, TypeCheck.Spec.Raw, {unquote(name), %{kind: unquote(call_kind), type: unquote(Macro.escape(raw_type))}})
+    end
+  end
+
+  defp build_spec_ast(args) do
+    {name, arg_types, return_type} = extract_spec_name(args)
+    quote do
+      Module.put_attribute(__MODULE__, TypeCheck.Spec.Raw, {unquote(name), %{kind: :spec, arg_types: unquote(Macro.escape(arg_types)), type: unquote(Macro.escape(return_type))}})
     end
   end
 
@@ -45,9 +46,12 @@ defmodule TypeCheck.Spec do
         raise "Expected type name to be a variable, but got `#{Macro.to_string(name)}` while parsing #{Macro.to_string(ast)}"
     end
   end
-
   defp extract_type_name(other) do
     raise "Expected a definition in the shape of `name :: type` but got `#{Macro.to_string(other)}`"
+  end
+
+  defp extract_spec_name(ast = {:"::", _, [{function_name, _, arg_types}, return_type]}) when is_atom(function_name) and is_list(arg_types) do
+    {function_name, arg_types, return_type}
   end
 
   defp extract_var_name({name, _, module}) when is_atom(name) and is_atom(module), do: {:ok, name}
