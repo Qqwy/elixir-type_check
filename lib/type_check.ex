cf89002855de55ec41d7a9887b84a2f59b75b16e
Marten/Qqwy <w-m@wmcode.nl>
2020-06-27 18:07:37 +0200

    manual conforming

--- lib/type_check.ex
+++ lib/type_check.ex
@@ -4,4 +4,31 @@ defmodule TypeCheck do
       use TypeCheck.Macros
     end
   end
+
+  defmacro conforms(value, type) do
+    check = TypeCheck.Protocols.ToCheck.to_check(type, Macro.var(:value, __MODULE__))
+    quote do
+      case unquote(check) do
+        :ok -> {:ok, value}
+        other -> other
+      end
+    end
+  end
+
+  defmacro conform?(value, type) do
+    check = TypeCheck.Protocols.ToCheck.to_check(type, Macro.var(:value, __MODULE__))
+    quote do
+      unquote(check) == :ok
+    end
+  end
+
+  defmacro conform!(value, type) do
+    check = TypeCheck.Protocols.ToCheck.to_check(type, Macro.var(:value, __MODULE__))
+    quote do
+      case unquote(check) do
+        :ok -> value
+        other -> raise ArgumentError, inspect(other)
+      end
+    end
+  end
 end
