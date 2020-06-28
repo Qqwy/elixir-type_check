14c497141177d0a043a966553325cb2882b03c30
Marten/Qqwy <w-m@wmcode.nl>
2020-06-28 11:54:35 +0200

    prevent double-nesting

--- lib/type_check/builtin.ex
+++ lib/type_check/builtin.ex
@@ -23,6 +23,12 @@ defmodule TypeCheck.Builtin do
     %TypeCheck.Builtin.List{element_type: a}
   end
 
+  # prevents double-expanding
+  # when called as `tuple([1,2,3])` by the user.
+  def tuple(list = %TypeCheck.Builtin.FixedList{}) do
+    tuple(list.element_types)
+  end
+
   def tuple(element_types_list) when is_list(element_types_list) do
     %TypeCheck.Builtin.Tuple{element_types: element_types_list}
   end
@@ -62,12 +68,27 @@ defmodule TypeCheck.Builtin do
     %TypeCheck.Builtin.Map{key_type: key_type, value_type: value_type}
   end
 
-  # TODO prevent double-nesting
+  def fixed_map(keywords)
+
+  # prevents double-expanding
+  # when called as `fixed_map(%{a: 1, b: 2})` by the user.
+  def fixed_map(map = %TypeCheck.Builtin.FixedMap{}) do
+    map
+  end
+
   def fixed_map(keywords) when is_map(keywords) or is_list(keywords) do
     %TypeCheck.Builtin.FixedMap{keypairs: Enum.into(keywords, [])}
   end
 
-  # TODO prevent double-nesting
+
+  def fixed_list(element_types)
+
+  # prevents double-expanding
+  # when called as `fixed_list([1,2,3])` by the user.
+  def fixed_list(list = %TypeCheck.Builtin.FixedList{}) do
+    list
+  end
+
   def fixed_list(element_types) when is_list(element_types) do
     %TypeCheck.Builtin.FixedList{element_types: element_types}
   end
