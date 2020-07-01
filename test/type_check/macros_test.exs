cce060f183eafc4a0fa46ace705a1ec0842e2136
Marten/Qqwy <w-m@wmcode.nl>
2020-07-01 17:14:58 +0200

    some _very_ rudimentary tests

---
+++ test/type_check/macros_test.exs
@@ -0,0 +1,28 @@
+defmodule TypeCheck.MacrosTest do
+  use ExUnit.Case
+  use ExUnitProperties
+  import StreamData, only: []
+
+  doctest TypeCheck.Macros
+
+  describe "basic type definition" do
+    defmodule BasicTypeDefinition do
+      use TypeCheck
+
+      type mylist :: list(integer())
+    end
+
+    test "is exported as function" do
+      assert {:mylist, 0} in BasicTypeDefinition.__info__(:functions)
+      assert TypeCheck.Type.is_type?(BasicTypeDefinition.mylist())
+    end
+
+    property "can be turned into a StreamData generator" do
+      gen = TypeCheck.Type.StreamData.gen(BasicTypeDefinition.mylist())
+      check all x <- gen do
+        assert is_list(x)
+        assert Enum.all?(x, fn elem -> is_integer(elem) end)
+      end
+    end
+  end
+end
--- test/type_check_test.exs
+++ test/type_check_test.exs
@@ -2,7 +2,4 @@ defmodule TypeCheckTest do
   use ExUnit.Case
   doctest TypeCheck
 
-  test "greets the world" do
-    assert TypeCheck.hello() == :world
-  end
 end
