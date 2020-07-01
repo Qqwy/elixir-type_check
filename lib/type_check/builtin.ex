0102c889ae6ebeee7c29654f48b4766babadb883
Marten/Qqwy <w-m@wmcode.nl>
2020-07-01 21:58:37 +0200

    Much more documentation of TypeCheck.Builtin

--- lib/type_check/builtin.ex
+++ lib/type_check/builtin.ex
@@ -6,83 +6,212 @@ defmodule TypeCheck.Builtin do
   """
 
   @doc typekind: :builtin
-  @doc "test"
+  @doc """
+  Any Elixir value.
+
+  Will always succeed.
+
+  c.f. `TypeCheck.Builtin.Any`
+  """
   def any() do
     %TypeCheck.Builtin.Any{}
   end
+
+  @doc typekind: :builtin
+  @doc "alias for `any/0`"
   def term(), do: any()
 
+  @doc typekind: :builtin
+  @doc """
+  Any Elixir atom.
+
+  c.f. `TypeCheck.Builtin.Atom`
+  """
   def atom() do
     %TypeCheck.Builtin.Atom{}
   end
+
+  @doc typekind: :builtin
+  @doc """
+  Any Elixir atom,
+  but indicates that the atom
+  is expected to be used as a module.
+
+  c.f. `atom/0`
+  """
   def module(), do: atom()
 
+  @doc typekind: :builtin
+  @doc """
+  The same as `type`,
+  but indicates that the result will be used
+  as a boolean.
+  """
   def as_boolean(type) do
     TypeCheck.Type.ensure_type!(type)
     type
   end
 
+  @doc typekind: :builtin
+  @doc """
+  Shorthand for `range(0..255)`
+  """
   def arity() do
     range(0..255)
   end
 
+  @doc typekind: :builtin
+  @doc """
+  Any binary.
+
+  A binary is a bitstring with a bitsize divisible by eight.
+
+  c.f. `TypeCheck.Builtin.Binary`
+  """
   def binary() do
     %TypeCheck.Builtin.Binary{}
   end
 
+  @doc typekind: :builtin
+  @doc """
+  Any bitstring
+
+  c.f. `TypeCheck.Builtin.Bitstring`
+  """
   def bitstring() do
     %TypeCheck.Builtin.Bitstring{}
   end
 
+  @doc typekind: :builtin
+  @doc """
+  Any boolean
+
+  (either `true` or `false`.)
+
+  c.f. `TypeCheck.Builtin.Boolean`
+  """
   def boolean() do
     %TypeCheck.Builtin.Boolean{}
   end
 
+  @doc typekind: :builtin
+  @doc """
+  A byte; shorthand for `range(0..255)`
+
+  c.f. `range/1`
+  """
   def byte() do
     range(0..255)
   end
 
+  @doc typekind: :builtin
+  @doc """
+  A char; shorthand for `range(0..0x10FFFF)`
+
+  c.f. `range/1`
+  """
   def char() do
     range(0..0x10FFFF)
   end
 
+  @doc typekind: :builtin
+  @doc """
+  A list filled with characters; exactly `list(char())`
+
+  c.f. `list/1` and `char/0`
+  """
   def charlist() do
     list(char())
   end
 
+  @doc typekind: :builtin
+  @doc """
+  Any function (of any arity), regardless of input or output types
+
+  c.f. `TypeCheck.Builtin.Function`
+  """
   def function() do
     %TypeCheck.Builtin.Function{}
   end
 
+  @doc typekind: :builtin
+  @doc """
+  Alias for `function/0`.
+  """
   def fun() do
     function()
   end
 
+  @doc typekind: :builtin
+  @doc """
+  Any integer.
+
+  C.f. `TypeCheck.Builtin.Integer`
+  """
   def integer() do
     %TypeCheck.Builtin.Integer{}
   end
 
+  @doc typekind: :builtin
+  @doc """
+  Any float.
+
+  C.f. `TypeCheck.Builtin.Integer`
+  """
   def float() do
     %TypeCheck.Builtin.Float{}
   end
 
+  @doc typekind: :builtin
+  @doc """
+  A (proper) list with any type of elements;
+
+  shorthand for `list(any())`
+
+  C.f. `list/1` and `any/0`
+  """
   def list() do
     list(any())
   end
 
+  @doc typekind: :builtin
+  @doc """
+  A (proper) list containing only elements of type `a`.
+
+  C.f. `TypeCheck.Builtin.List`
+  """
   def list(a) do
     TypeCheck.Type.ensure_type!(a)
     %TypeCheck.Builtin.List{element_type: a}
   end
+  @doc typekind: :builtin
+  @doc """
+  A module-function-arity tuple
 
+  - Module is a `module/0`
+  - function is an `atom/0`
+  - Arity is an `arity/0`
+
+  C.f. `tuple_of/1`
+  """
   def mfa() do
     tuple_of([module(), atom(), arity()])
   end
 
   @type tuple_of(_list_of_elements) :: tuple()
 
-  @doc typekind: :extension
-  @doc "bla"
+  @doc typekind: :builtin
+  @doc """
+  A tuple whose elements are of the types given by `list_of_element_types`.
+
+  Desugaring of writing tuples directly in your types:
+  `{a, b, c}` desugars to `tuple_of([a, b, c])`.
+
+  Represented in Elixir's builtin Typespecs as a plain tuple,
+  where each of the elements are the respective element of `list_of_types`.
+
+  C.f. `TypeCheck.Builtin.Tuple`
+  """
   def tuple_of(list_of_element_types)
   # prevents double-expanding
   # when called as `tuple_of([1,2,3])` by the user.
@@ -103,14 +232,36 @@ defmodule TypeCheck.Builtin do
     tuple_of(elems)
   end
 
+  @doc typekind: :builtin
+  @doc """
+  A literal value.
+
+  Desugaring of using any literal primitive value
+  (like a particular integer, float, atom, binary or bitstring)
+  directly a type.
+
+  For instance, `10` desugars to `literal(10)`.
+
+  Represented in Elixir's builtin Typespecs as the primitive value itself.
+
+  C.f. `TypeCheck.Builtin.Literal`
+  """
   def literal(value) do
     %TypeCheck.Builtin.Literal{value: value}
   end
 
+  @doc false
   def left | right do
     one_of(left, right)
   end
 
+  @doc typekind: :builtin
+  @doc """
+  A union of multiple types (also known as a 'sum type')
+
+  Desugaring of `a | b` and `a | b | c | d`.
+  (and represented that way in Elixir's builtin Typespecs).
+  """
   def one_of(left, right)
 
   # Prevents nesting
@@ -140,18 +291,50 @@ defmodule TypeCheck.Builtin do
     %TypeCheck.Builtin.OneOf{choices: list_of_possibilities}
   end
 
+  @doc typekind: :builtin
+  @doc """
+  Any integer in the half-open range `range`.
+
+  Desugaring of `a..b`.
+  (And represented that way in Elixir's builtin Typespecs.)
+
+  C.f. `TypeCheck.Builtin.Range`
+  """
   def range(range = _lower.._higher) do
     %TypeCheck.Builtin.Range{range: range}
   end
 
+  @doc typekind: :builtin
+  @doc """
+  Any integer between `lower` (includsive) and `higher` (exclusive).
+
+  Desugaring of `lower..higher`.
+  (And represented that way in Elixir's builtin Typespecs.)
+
+  C.f. `range/1`
+  """
   def range(lower, higher) do
     %TypeCheck.Builtin.Range{range: lower..higher}
   end
 
+  @doc typekind: :builtin
+  @doc """
+  Any Elixir map with any types as keys and any types as values.
+
+  C.f. `TypeCheck.Builtin.Map`
+  """
   def map() do
     %TypeCheck.Builtin.Map{key_type: any(), value_type: any()}
   end
 
+  @doc typekind: :extension
+  @doc """
+  Any map containing zero or more keys of `key_type` and values of `value_type`.
+
+  Represented in Elixir's builtin Typespecs as `%{optional(key_type) => value_type}`.
+
+  C.f. `TypeCheck.Builtin.Map`
+  """
   def map(key_type, value_type) do
     TypeCheck.Type.ensure_type!(key_type)
     TypeCheck.Type.ensure_type!(value_type)
@@ -159,6 +342,19 @@ defmodule TypeCheck.Builtin do
     %TypeCheck.Builtin.Map{key_type: key_type, value_type: value_type}
   end
 
+  @doc typekind: :builtin
+  @doc """
+  A map with exactly the key-value-pairs indicated by `keywords`
+
+  where all keys are required to be literal values,
+  and the values are a type specification.
+
+  Desugaring of `%{key: value_type, "other_key" => value_type2}`.
+
+  Represented in Elixir's builtin Typespecs as
+  `%{required(:key1) => value_type1, required("other key") => value_type2}`.
+  (for e.g. a call to `fixed_map([key1: value_type1, {"other key", value_type2}])`)
+  """
   def fixed_map(keywords)
 
   # prevents double-expanding
@@ -187,6 +383,7 @@ defmodule TypeCheck.Builtin do
 
     %TypeCheck.Builtin.FixedMap{keypairs: Enum.into(keywords, [])}
   end
+
 
   def fixed_list(element_types)
 
