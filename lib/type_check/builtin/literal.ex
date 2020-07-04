025690ae249a0be35ce489aa62d294410801a825
Marten/Qqwy <w-m@wmcode.nl>
2020-07-05 00:25:46 +0200

    WIP; when-clause seems to be broken

--- lib/type_check/builtin/literal.ex
+++ lib/type_check/builtin/literal.ex
@@ -3,7 +3,7 @@ defmodule TypeCheck.Builtin.Literal do
 
   use TypeCheck
   type t :: %__MODULE__{value: literal :: any()}
-  type problem_tuple :: {t(), :not_same_value, map(), val :: any()} when literal !== value
+  type problem_tuple :: {t(), :not_same_value, map(), val :: any()}
 
   defimpl TypeCheck.Protocols.ToCheck do
     def to_check(s = %{value: value}, param) do
