29969497d7b2f823ca51ae4e5879230661a56283
Marten/Qqwy <w-m@wmcode.nl>
2020-07-07 11:53:04 +0200

    Setting up mix.exs

--- mix.exs
+++ mix.exs
@@ -1,6 +1,8 @@
 defmodule TypeCheck.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/Qqwy/elixir-type_check"
+
   def project do
     [
       app: :type_check,
@@ -8,24 +10,12 @@ defmodule TypeCheck.MixProject do
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      description: description(),
       name: "TypeCheck",
-      source_url: "https://github.com/Qqwy/elixir-type_check",
+      package: package(),
+      source_url: @source_url,
       homepage_url: "https://github.com/Qqwy/elixir-type_check",
-      docs: [
-        main: "TypeCheck",
-        groups_for_modules: [
-          "Main": [TypeCheck, TypeCheck.Macros, TypeCheck.Type, TypeCheck.Spec],
-          "Errors and Formatting them": ~r"^TypeCheck.TypeError",
-          "Property Testing": ~r"^TypeCheck.Type.StreamData",
-          "Builtin Types": ~r"^TypeCheck.Builtin",
-          "Other": ~r"^.*"
-        ],
-        nest_modules_by_prefix: [TypeCheck.Builtin, TypeCheck.TypeError],
-        groups_for_functions: [
-          "Built-in Elixir types": &(&1[:typekind] == :builtin),
-          "Extensions": &(&1[:typekind] == :extension),
-        ]
-      ]
+      docs: docs()
     ]
   end
 
@@ -36,6 +26,22 @@ defmodule TypeCheck.MixProject do
     ]
   end
 
+  defp description do
+    """
+    Fast and flexible runtime type-checking.
+    """
+  end
+
+  defp package do
+    [
+      name: :type_check,
+      files: ["lib", "mix.exs", "README*", "LICENSE"],
+      maintainers: ["Wiebe-Marten Wijnja/Qqwy"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
@@ -43,6 +49,27 @@ defmodule TypeCheck.MixProject do
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:stream_data, "~> 0.5.0", optional: true},
       {:ex_doc, "~> 0.22", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      logo: "media/type_check_logo_icon_flat_small.svg",
+      extras: ["README.md"],
+      # main: "TypeCheck",
+      groups_for_modules: [
+        "Main": [TypeCheck, TypeCheck.Macros, TypeCheck.Type, TypeCheck.Spec],
+        "Errors and Formatting them": ~r"^TypeCheck.TypeError",
+        "Property Testing": ~r"^TypeCheck.Type.StreamData",
+        "Builtin Types": ~r"^TypeCheck.Builtin",
+        "Other": ~r"^.*"
+      ],
+      nest_modules_by_prefix: [TypeCheck.Builtin, TypeCheck.TypeError],
+      groups_for_functions: [
+        "Built-in Elixir types": &(&1[:typekind] == :builtin),
+        "Extensions": &(&1[:typekind] == :extension),
+      ]
     ]
   end
 end
