0eb4a31766730c4696f063075f9ec1b4a92595a4
Marten/Qqwy <w-m@wmcode.nl>
2020-07-07 13:41:04 +0200

    improve formatter settings

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,11 @@
 # Used by "mix format"
+no_parens = [type: 1, typep: 1, opaque: 1, spec: 1]
+
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  import_depts: [:stream_data]
+  locals_without_parens: no_parens,
+  import_deps: [:stream_data],
+  export: [
+    locals_without_parens: no_parens
+  ]
 ]
