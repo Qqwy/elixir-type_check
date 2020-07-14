245a8ddef5393d5573ca86e2987cbe53c934c724
Marten/Qqwy <w-m@wmcode.nl>
2020-07-14 15:30:06 +0200

    fix indentation of doctest

--- lib/type_check/type/stream_data.ex
+++ lib/type_check/type/stream_data.ex
@@ -74,22 +74,21 @@ defmodule TypeCheck.Type.StreamData do
     When given a type, it is transformed to a StreamData generator
     that can be used in a property test.
 
-    iex> import TypeCheck.Type.StreamData
-    iex> generator = TypeCheck.Type.build({:ok | :error, integer()}) |> to_gen()
-    iex> StreamData.seeded(generator, 42) |> Enum.take(10)
-    [
-    {:ok, -1},
-    {:ok, 2},
-    {:ok, -2},
-    {:ok, -4},
-    {:ok, 1},
-    {:ok, 1},
-    {:ok, 2},
-    {:ok, 4},
-    {:ok, -7},
-    {:ok, 5}
-    ]
-
+        iex> import TypeCheck.Type.StreamData
+        iex> generator = TypeCheck.Type.build({:ok | :error, integer()}) |> to_gen()
+        iex> StreamData.seeded(generator, 42) |> Enum.take(10)
+        [
+        {:ok, -1},
+        {:ok, 2},
+        {:ok, -2},
+        {:ok, -4},
+        {:ok, 1},
+        {:ok, 1},
+        {:ok, 2},
+        {:ok, 4},
+        {:ok, -7},
+        {:ok, 5}
+        ]
     """
     def to_gen(type) do
       TypeCheck.Protocols.ToStreamData.to_gen(type)
