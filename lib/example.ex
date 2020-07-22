1b82082b967c08b780cf087be38d1b2db2889ff0
Marten/Qqwy <w-m@wmcode.nl>
2020-07-22 15:56:24 +0200

    Removes example file

--- lib/example.ex
+++
@@ -1,9 +0,0 @@
-defmodule Example do
-  use TypeCheck
-  # @type! nice_num :: (x :: non_neg_integer() when x != 10)
-
-  @spec! in_magic_range((x :: non_neg_integer() when x != 42)) :: boolean()
-  def in_magic_range(val) do
-    true
-  end
-end
