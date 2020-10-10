b6d73dc7d8778ff2e8701ee238fa849fdef00c31
Marten/Qqwy <w-m@wmcode.nl>
2020-10-10 17:27:27 +0200

    Ensure `:stream_data` is not in `:extra_applications`
    Fixes #27

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule TypeCheck.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :stream_data]
+      extra_applications: [:logger]
     ]
   end
 
