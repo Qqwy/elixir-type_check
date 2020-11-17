fdfaf74553cbd475c7a826b8e96918d2ac4e4a80
Marten/Qqwy <w-m@wmcode.nl>
2020-11-17 22:16:29 +0100

    Support unquote fragments in `test!`, `spec!` and co.
    Fixes #36

--- test/type_check/macros_test.exs
+++ test/type_check/macros_test.exs
@@ -26,5 +26,32 @@ defmodule TypeCheck.MacrosTest do
         assert Enum.all?(x, fn elem -> is_integer(elem) end)
       end
     end
+
+    test "support unquote fragments" do
+      defmodule UnquoteFragmentSpec do
+        defmacro my_macro(name) do
+          quote bind_quoted: [name: name], location: :keep do
+
+            use TypeCheck
+            spec! unquote(:"my_function_#{name}")(binary) :: binary
+            def unquote(:"my_function_#{name}")(greeting) do
+              greeting
+            end
+          end
+        end
+      end
+
+      defmodule UnquoteFragmentSpecExample do
+        require UnquoteFragmentSpec
+
+        UnquoteFragmentSpec.my_macro("greeter")
+      end
+
+      assert TypeCheck.Spec.defined?(UnquoteFragmentSpecExample, :my_function_greeter, 1)
+      assert UnquoteFragmentSpecExample.my_function_greeter("hi") == "hi"
+      assert_raise(TypeCheck.TypeError, fn ->
+        UnquoteFragmentSpecExample.my_function_greeter(1)
+      end)
+    end
   end
 end
