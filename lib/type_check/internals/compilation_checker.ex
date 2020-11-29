7b4eab99bc9ce552e67ed550ad7b28e5a2ee1008
Marten/Qqwy <w-m@wmcode.nl>
2020-11-29 22:02:03 +0100

    checking by (de)compiling...

---
+++ lib/type_check/internals/compilation_checker.ex
@@ -0,0 +1,16 @@
+defmodule TypeCheck.CompilationChecker do
+  def __on_definition__(_env, kind, name, args, guards, body) do
+    args_str =
+      args
+      |> Enum.map(&inspect/1)
+      |> Enum.join(", ")
+
+    IO.puts("#{kind} #{name}(#{args_str}) when #{inspect(guards)} do")
+    # IO.inspect(args)
+    # IO.puts("and guards")
+    # IO.inspect(guards)
+    # IO.puts()
+    IO.puts(Macro.to_string(body[:do]))
+    IO.puts("end")
+  end
+end
