14884478045a6a1f215da624b993a89eb2171907
Marten/Qqwy <w-m@wmcode.nl>
2021-09-18 23:41:03 +0200

    Remove 'Bootstrap' module (but keep file; it just did not need to be a module)

--- lib/type_check/internals/bootstrap.ex
+++ lib/type_check/internals/bootstrap.ex
@@ -1,16 +1,13 @@
-defmodule TypeCheck.Internals.Bootstrap do
-  require __MODULE__.Macros
-  # Waiting for TypeCheck.Builtin.* to be compiled
-  # ensures deterministic compilation
-  # (otherwise compilation might deadlock).
-  # A bit of a hack, suggestions are welcome.
-  case Code.ensure_compiled(TypeCheck.Builtin.Any) do
-    {:error, problem} -> IO.puts(problem)
-    {:module, _} ->
+require TypeCheck.Internals.Bootstrap.Macros
+# Waiting for TypeCheck.Builtin.* to be compiled
+# ensures deterministic compilation
+# (otherwise compilation might deadlock).
+# A bit of a hack, suggestions are welcome.
+case Code.ensure_compiled(TypeCheck.Builtin.Any) do
+  {:error, problem} -> IO.puts(problem)
+  {:module, _} ->
 
-      __MODULE__.Macros.recompile(TypeCheck.Type, "lib/type_check/type.ex")
-      __MODULE__.Macros.recompile(TypeCheck.Options, "lib/type_check/options.ex")
-      __MODULE__.Macros.recompile(TypeCheck.Builtin, "lib/type_check/builtin.ex")
-
-  end
+    TypeCheck.Internals.Bootstrap.Macros.recompile(TypeCheck.Type, "lib/type_check/type.ex")
+    TypeCheck.Internals.Bootstrap.Macros.recompile(TypeCheck.Options, "lib/type_check/options.ex")
+    TypeCheck.Internals.Bootstrap.Macros.recompile(TypeCheck.Builtin, "lib/type_check/builtin.ex")
 end
