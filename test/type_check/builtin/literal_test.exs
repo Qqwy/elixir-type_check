4738b735f3ea06e04f67fcfa230ca55ed5c35fe2
Marten/Qqwy <w-m@wmcode.nl>
2021-09-19 22:25:22 +0200

    Extra literal inspect test.

---
+++ test/type_check/builtin/literal_test.exs
@@ -0,0 +1,12 @@
+defmodule TypeCheck.Builtin.LiteralTest do
+  use ExUnit.Case
+  use ExUnitProperties
+  import StreamData, only: []
+  import TypeCheck.Builtin
+  require TypeCheck.Type
+
+  test "inspects literal range-structs correctly" do
+    res = inspect(TypeCheck.Type.build(literal(1..5)))
+    assert "#TypeCheck.Type< literal( 1..5 ) >" == res
+  end
+end
