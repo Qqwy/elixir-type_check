3ede855bbf7993fc1352b10c540c43296c993ca5
Marten/Qqwy <w-m@wmcode.nl>
2021-09-25 00:14:46 +0200

    fix range type

--- lib/type_check/options/default_overrides/range.ex
+++ lib/type_check/options/default_overrides/range.ex
@@ -2,14 +2,11 @@ defmodule TypeCheck.Options.DefaultOverrides.Range do
   use TypeCheck
   @type! limit() :: integer()
 
-  @type! step() :: pos_integer() | neg_integer()
-
-  @type! t() :: %Elixir.Range{first: limit(), last: limit(), step: step()}
-
-
   if Version.compare(System.version(), "1.12.0") == :lt do
     @type! t(first, last) :: %Elixir.Range{first: first, last: last}
   else
+    @type! step() :: pos_integer() | neg_integer()
+
     @type! t(first, last) :: %Elixir.Range{first: first, last: last, step: step()}
   end
 
