f4e9621d734cc833d6207061540cd18c40e54a1b
Marten/Qqwy <w-m@wmcode.nl>
2021-10-12 13:09:15 +0200

    WIP

--- lib/type_check/builtin/function.ex
+++ lib/type_check/builtin/function.ex
@@ -1,28 +1,45 @@
 defmodule TypeCheck.Builtin.Function do
-  defstruct []
+  defstruct [param_types: nil, return_type: %TypeCheck.Builtin.Any{}]
 
   use TypeCheck
-  @opaque! t :: %TypeCheck.Builtin.Function{}
-  @type! problem_tuple :: {t(), :no_match, %{}, any()}
+  @opaque! t :: %TypeCheck.Builtin.Function{
+    param_types: list(TypeCheck.Type.t() | nil),
+    return_type: TypeCheck.Type.t()
+  }
+  @type! problem_tuple :: {t(), :no_match, %{}, any()} # TODO
 
   defimpl TypeCheck.Protocols.ToCheck do
     def to_check(s, param) do
-      quote generated: true, location: :keep do
-        case unquote(param) do
-          x when is_function(x) ->
-            {:ok, []}
+      if s.param_types == nil && s.return_type == %TypeCheck.Builtin.Any{} do
+        quote generated: true, location: :keep do
+          case unquote(param) do
+            x when is_function(x) ->
+              {:ok, []}
 
-          _ ->
-            {:error, {unquote(Macro.escape(s)), :no_match, %{}, unquote(param)}}
+            _ ->
+              {:error, {unquote(Macro.escape(s)), :no_match, %{}, unquote(param)}}
+          end
+        end
+      else
+        # TODO
+        quote generated: true, location: :keep do
+          {:ok, []}
         end
       end
     end
   end
 
   defimpl TypeCheck.Protocols.Inspect do
-    def inspect(_, opts) do
-      "function()"
-      |> Inspect.Algebra.color(:builtin_type, opts)
+    import Kernel, except: [inspect: 2]
+    def inspect(s, opts) do
+      case s do
+        %TypeCheck.Builtin.Function{param_types: nil, return_type: %TypeCheck.Builtin.Any{}} ->
+          "function()"
+          |> Inspect.Algebra.color(:builtin_type, opts)
+        other ->
+          # TODO
+          Kernel.inspect(other, structs: false)
+      end
     end
   end
 
