129d9c8852cc613f56714a0109cd142247417971
Marten/Qqwy <w-m@wmcode.nl>
2021-10-16 20:42:09 +0200

    Generate functions which return the same pseudorandom answer for the same input.

--- mix.exs
+++ mix.exs
@@ -65,6 +65,7 @@ defmodule TypeCheck.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:murmur, "~> 1.0"},
       {:stream_data, "~> 0.5.0", optional: true},
       {:ex_doc, "~> 0.22", only: :docs, runtime: false},
       {:dialyxir, "~> 1.0", only: [:dialize], runtime: false},
