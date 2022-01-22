9dcc6dff15c83cf138d253df8dfb72da89a7e011
Marten/Qqwy <w-m@wmcode.nl>
2022-01-22 01:32:00 +0100

    expanded 'trivial example'

--- lib/example.ex
+++ lib/example.ex
@@ -1,3 +1,20 @@
+# defmodule ExKeymap.KeymapItem do
+#   use TypeCheck, debug: true
+#   defstruct [:help]
+
+#   @type! t() :: %__MODULE__{
+#     help: binary() | nil
+#   }
+
+#   # @spec! new(String.t(), (-> any()), String.t() | nil) :: t()
+#   @spec! new(binary() | nil) :: t()
+#   def new(help) do
+#     %__MODULE__{
+#       help: help
+#     }
+#   end
+# end
+
 defmodule ExKeymap.KeymapItem do
   use TypeCheck
   defstruct [:item_name, :help, :fun]
@@ -5,7 +22,7 @@ defmodule ExKeymap.KeymapItem do
   @type! t :: %__MODULE__{
     item_name: String.t(),
     fun: (-> any()),
-    help: String.t() | nil
+  help: String.t() | nil
   }
 
   @spec! new(String.t(), (-> any()), String.t() | nil) :: t()
