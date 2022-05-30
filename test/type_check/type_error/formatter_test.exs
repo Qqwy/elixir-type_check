9c324d450f2f019ab9bd19646c3f25791db20748
gram <mail@orsinium.dev>
2022-05-30 11:20:51 +0200

    actualize formatter tests

--- test/type_check/type_error/formatter_test.exs
+++ test/type_check/type_error/formatter_test.exs
@@ -5,36 +5,46 @@ defmodule TypeCheck.TypeError.FormatterTest do
 
   import TypeCheck.Type.StreamData
 
-  for module <- [TypeCheck.Builtin.Atom,
-                 TypeCheck.Builtin.Binary,
-                 TypeCheck.Builtin.Bitstring,
-                 TypeCheck.Builtin.Boolean,
-                 TypeCheck.Builtin.FixedList,
-                 TypeCheck.Builtin.FixedMap,
-                 TypeCheck.Builtin.FixedTuple,
-                 TypeCheck.Builtin.Float,
-                 TypeCheck.Builtin.Integer,
-                 TypeCheck.Builtin.Lazy,
-                 TypeCheck.Builtin.List,
-                 TypeCheck.Builtin.Literal,
-                 TypeCheck.Builtin.Map,
-                 TypeCheck.Builtin.NamedType,
-                 TypeCheck.Builtin.NegInteger,
-                 TypeCheck.Builtin.NonNegInteger,
-                 TypeCheck.Builtin.None,
-                 TypeCheck.Builtin.Number,
-                 TypeCheck.Builtin.OneOf,
-                 TypeCheck.Builtin.PosInteger,
-                 TypeCheck.Builtin.Range,
-                 TypeCheck.Builtin.Tuple,
-                 TypeCheck.Builtin.Function,
-                ] do
+  for module <- [
+        # TypeCheck.Builtin.Any,
+        TypeCheck.Builtin.Atom,
+        TypeCheck.Builtin.Binary,
+        TypeCheck.Builtin.Bitstring,
+        TypeCheck.Builtin.Boolean,
+        TypeCheck.Builtin.CompoundFixedMap,
+        TypeCheck.Builtin.FixedList,
+        TypeCheck.Builtin.FixedMap,
+        TypeCheck.Builtin.FixedTuple,
+        TypeCheck.Builtin.Float,
+        TypeCheck.Builtin.Function,
+        # TypeCheck.Builtin.Guarded,
+        TypeCheck.Builtin.ImplementsProtocol,
+        TypeCheck.Builtin.Integer,
+        TypeCheck.Builtin.Lazy,
+        TypeCheck.Builtin.List,
+        TypeCheck.Builtin.Literal,
+        TypeCheck.Builtin.Map,
+        TypeCheck.Builtin.MaybeImproperList,
+        TypeCheck.Builtin.NamedType,
+        TypeCheck.Builtin.NegInteger,
+        TypeCheck.Builtin.None,
+        TypeCheck.Builtin.NonNegInteger,
+        TypeCheck.Builtin.Number,
+        TypeCheck.Builtin.OneOf,
+        # TypeCheck.Builtin.Pid,
+        TypeCheck.Builtin.Port,
+        TypeCheck.Builtin.PosInteger,
+        TypeCheck.Builtin.Range,
+        TypeCheck.Builtin.Reference,
+        # TypeCheck.Builtin.SizedBitstring,
+        TypeCheck.Builtin.Tuple
+      ] do
     property "the default formatter is able to handle all problem tuples (returning a binary string message) of type #{module}" do
       check all problem <-
-      StreamData.scale(
-        to_gen(unquote(module).problem_tuple()),
-        &div(&1, 3)
-      ) do
+                  StreamData.scale(
+                    to_gen(unquote(module).problem_tuple()),
+                    &div(&1, 3)
+                  ) do
         result = TypeCheck.TypeError.DefaultFormatter.format(problem)
         assert is_binary(result)
       end
