a4184b231e45e6297f9067d03961f87e0e94e478
Marten/Qqwy <w-m@wmcode.nl>
2022-06-13 14:28:32 +0200

    Escape functions

--- lib/type_check/builtin/function.ex
+++ lib/type_check/builtin/function.ex
@@ -1,20 +1,22 @@
 defmodule TypeCheck.Builtin.Function do
-  defstruct [param_types: nil, return_type: %TypeCheck.Builtin.Any{}]
+  defstruct param_types: nil, return_type: %TypeCheck.Builtin.Any{}
 
   use TypeCheck
-  @opaque! t :: %TypeCheck.Builtin.Function{
-    param_types: list(TypeCheck.Type.t()) | nil,
-    return_type: TypeCheck.Type.t()
-  }
-  @type! problem_tuple :: {t(), :no_match, %{}, any()}
 
+  @opaque! t :: %TypeCheck.Builtin.Function{
+             param_types: list(TypeCheck.Type.t()) | nil,
+             return_type: TypeCheck.Type.t()
+           }
+  @type! problem_tuple :: {t(), :no_match, %{}, any()}
 
   defimpl TypeCheck.Protocols.Escape do
     def escape(s) do
-      # update_in(s.element_types, &Enum.map(&1, fn val -> escape(val) end))
-      require Logger
-      Logger.warn("Todo: escaping functions")
-      s # TODO!
+      s
+      |> Map.update!(:param_types, fn
+        nil -> nil
+        list when is_list(list) -> Enum.map(list, &TypeCheck.Protocols.Escape.escape(&1))
+      end)
+      |> Map.update!(:return_type, &TypeCheck.Protocols.Escape.escape(&1))
     end
   end
 
@@ -22,10 +24,12 @@ defmodule TypeCheck.Builtin.Function do
     def to_check(s, param) do
       quote generated: true, location: :keep do
         p = unquote(param)
+
         case p do
           unquote(is_function_check(s)) ->
             wrapped_fun = unquote(@for.contravariant_wrapper(s, param))
             {:ok, [], wrapped_fun}
+
           _ ->
             {:error, {unquote(Macro.escape(s)), :no_match, %{}, unquote(param)}}
         end
@@ -38,6 +42,7 @@ defmodule TypeCheck.Builtin.Function do
           quote generated: true, location: :keep do
             x when is_function(x)
           end
+
         list ->
           quote generated: true, location: :keep do
             x when is_function(x, unquote(length(list)))
@@ -50,50 +55,70 @@ defmodule TypeCheck.Builtin.Function do
     case s do
       %{param_types: nil, return_type: %TypeCheck.Builtin.Any{}} ->
         original
+
       %{param_types: [], return_type: %TypeCheck.Builtin.Any{}} ->
         original
+
       %{param_types: nil, return_type: return_type} ->
-        quote generated: true, location: :keep, bind_quoted: [fun: original, s: Macro.escape(s), return_type: Macro.escape(return_type)] do
+        quote generated: true,
+              location: :keep,
+              bind_quoted: [
+                fun: original,
+                s: Macro.escape(s),
+                return_type: Macro.escape(return_type)
+              ] do
           {:arity, arity} = Function.info(fun, :arity)
           clean_params = Macro.generate_arguments(arity, __MODULE__)
-          return_code_check = TypeCheck.Protocols.ToCheck.to_check(return_type, Macro.var(:result, nil))
+
+          return_code_check =
+            TypeCheck.Protocols.ToCheck.to_check(return_type, Macro.var(:result, nil))
 
           wrapper_ast =
             quote do
               fn unquote_splicing(clean_params) ->
                 var!(result, nil) = var!(fun).(unquote_splicing(clean_params))
+
                 case unquote(return_code_check) do
                   {:ok, _bindings, altered_return_value} ->
                     altered_return_value
+
                   {:error, problem} ->
                     raise TypeCheck.TypeError,
-                    {unquote(Macro.escape(s)), :return_error,
-                    %{problem: problem, arguments: unquote(clean_params)}, var!(result, nil)}
+                          {unquote(Macro.escape(s)), :return_error,
+                           %{problem: problem, arguments: unquote(clean_params)},
+                           var!(result, nil)}
                 end
               end
             end
+
           {fun, _} = Code.eval_quoted(wrapper_ast, [fun: fun], __ENV__)
 
           fun
         end
+
       %{param_types: [], return_type: return_type} ->
-        return_code_check = TypeCheck.Protocols.ToCheck.to_check(return_type, Macro.var(:result, nil))
+        return_code_check =
+          TypeCheck.Protocols.ToCheck.to_check(return_type, Macro.var(:result, nil))
 
         quote generated: true, location: :keep do
           fn ->
             var!(result, nil) = unquote(original).()
+
             case unquote(return_code_check) do
               {:ok, _bindings, altered_return_value} ->
                 altered_return_value
+
               {:error, problem} ->
                 raise TypeCheck.TypeError,
-                  {unquote(Macro.escape(s)), :return_error,
-                  %{problem: problem, arguments: []}, var!(result, nil)}
+                      {unquote(Macro.escape(s)), :return_error,
+                       %{problem: problem, arguments: []}, var!(result, nil)}
             end
           end
         end
+
       %{param_types: param_types, return_type: return_type} ->
         clean_params = Macro.generate_arguments(length(param_types), __MODULE__)
+
         param_checks =
           param_types
           |> Enum.zip(clean_params)
@@ -102,7 +127,8 @@ defmodule TypeCheck.Builtin.Function do
             param_check_code(param_type, clean_param, index)
           end)
 
-        return_code_check = TypeCheck.Protocols.ToCheck.to_check(return_type, Macro.var(:result, nil))
+        return_code_check =
+          TypeCheck.Protocols.ToCheck.to_check(return_type, Macro.var(:result, nil))
 
         quote do
           fn unquote_splicing(clean_params) ->
@@ -112,17 +138,20 @@ defmodule TypeCheck.Builtin.Function do
               case unquote(return_code_check) do
                 {:ok, _bindings, altered_return_value} ->
                   altered_return_value
+
                 {:error, problem} ->
                   raise TypeCheck.TypeError,
-                    {unquote(Macro.escape(s)), :return_error,
-                     %{problem: problem, arguments: unquote(clean_params)}, var!(result, nil)}
+                        {unquote(Macro.escape(s)), :return_error,
+                         %{problem: problem, arguments: unquote(clean_params)}, var!(result, nil)}
               end
             else
               {{:error, problem}, index, param_type} ->
                 raise TypeCheck.TypeError,
-                {
-                  {unquote(Macro.escape(s)), :param_error,
-                   %{index: index, problem: problem}, unquote(clean_params)}, []}
+                      {
+                        {unquote(Macro.escape(s)), :param_error,
+                         %{index: index, problem: problem}, unquote(clean_params)},
+                        []
+                      }
             end
           end
         end
@@ -131,9 +160,11 @@ defmodule TypeCheck.Builtin.Function do
 
   def param_check_code(param_type, clean_param, index) do
     impl = TypeCheck.Protocols.ToCheck.to_check(param_type, clean_param)
+
     quote generated: true, location: :keep do
       [
-        {{:ok, _bindings, altered_param}, _index, _param_type} <- {unquote(impl), unquote(index), unquote(Macro.escape(param_type))},
+        {{:ok, _bindings, altered_param}, _index, _param_type} <-
+          {unquote(impl), unquote(index), unquote(Macro.escape(param_type))},
         clean_param = altered_param
       ]
     end
@@ -145,6 +176,7 @@ defmodule TypeCheck.Builtin.Function do
         %{param_types: nil, return_type: %TypeCheck.Builtin.Any{}} ->
           "function()"
           |> Inspect.Algebra.color(:builtin_type, opts)
+
         %{param_types: nil, return_type: return_type} ->
           inspected_return_type = TypeCheck.Protocols.Inspect.inspect(return_type, opts)
 
@@ -153,6 +185,7 @@ defmodule TypeCheck.Builtin.Function do
           |> Inspect.Algebra.glue(Inspect.Algebra.color("->", :builtin_type, opts))
           |> Inspect.Algebra.glue(inspected_return_type)
           |> Inspect.Algebra.concat(Inspect.Algebra.color(")", :builtin_type, opts))
+
         %{param_types: types, return_type: return_type} ->
           inspected_param_types =
             types
@@ -182,6 +215,7 @@ defmodule TypeCheck.Builtin.Function do
             |> StreamData.bind(fn {arity, seed} ->
               create_wrapper(result_type, arity, seed)
             end)
+
           %{param_types: param_types, return_type: result_type} when is_list(param_types) ->
             arity = length(param_types)
 
@@ -194,18 +228,20 @@ defmodule TypeCheck.Builtin.Function do
 
       defp create_wrapper(result_type, arity, hash_seed) do
         clean_params = Macro.generate_arguments(arity, __MODULE__)
+
         wrapper_ast =
           quote do
-          fn unquote_splicing(clean_params) ->
-            persistent_seed = :erlang.phash2(unquote(clean_params), unquote(hash_seed))
+            fn unquote_splicing(clean_params) ->
+              persistent_seed = :erlang.phash2(unquote(clean_params), unquote(hash_seed))
 
-            unquote(Macro.escape(result_type))
-            |> TypeCheck.Protocols.ToStreamData.to_gen()
-            |> StreamData.seeded(persistent_seed)
-            |> Enum.take(1)
-            |> List.first
+              unquote(Macro.escape(result_type))
+              |> TypeCheck.Protocols.ToStreamData.to_gen()
+              |> StreamData.seeded(persistent_seed)
+              |> Enum.take(1)
+              |> List.first()
+            end
           end
-        end
+
         {fun, _} = Code.eval_quoted(wrapper_ast)
         StreamData.constant(fun)
       end
