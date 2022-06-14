1b024b6d4b869361de250af2511d8360479c92dc
Marten/Qqwy <w-m@wmcode.nl>
2022-06-14 15:14:33 +0200

    Remove commented code

--- lib/type_check/internals/escaper.ex
+++ lib/type_check/internals/escaper.ex
@@ -10,39 +10,4 @@ defmodule TypeCheck.Internals.Escaper do
     |> TypeCheck.Protocols.Escape.escape()
     |> Macro.escape(unquote: :true)
   end
-
-  # def escape(val) do
-  #   case do_escape(val) do
-  #     map = %{} ->
-  #       Macro.escape(map, unquote: true)
-  #     other ->
-  #       other
-  #   end
-  # end
-
-  # def do_escape(s = %TypeCheck.Builtin.NamedType{}) do
-  #   case s do
-  #     %{called_as: {module, function, args}, type_kind: kind} when kind in [:type, :opaque] ->
-  #       escaped_args = args
-  #       |> do_escape()
-  #       |> Macro.escape()
-
-  #       quote do
-  #         unquote(module).unquote(function)(unquote_splicing(escaped_args))
-  #       end
-  #     other -> other
-  #   end
-  # end
-
-  # def do_escape(other_struct_or_map = %{}) do
-  #   :maps.map(fn _key, value -> do_escape(value) end, other_struct_or_map)
-  # end
-
-  # def do_escape(list) when is_list(list) do
-  #   Enum.map(list, fn value -> do_escape(value) end)
-  # end
-
-  # def do_escape(other) do
-  #   other
-  # end
 end
