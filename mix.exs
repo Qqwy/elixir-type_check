d108373affaed0b098cacfbea8cf52dc968d1247
Marten/Qqwy <w-m@resilia.nl>
2022-06-30 10:44:19 +0200

    Bump version and amend changelog

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TypeCheck.MixProject do
   def project do
     [
       app: :type_check,
-      version: "0.12.0",
+      version: "0.12.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
