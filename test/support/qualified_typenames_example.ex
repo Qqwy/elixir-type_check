8c0938664015a69caa86ba698c3e62db187d0a56
Marten/Qqwy <w-m@resilia.nl>
2022-10-15 19:05:55 +0200

    Fix the compilation order of the test support file.

--- test/support/qualified_typenames_example.ex
+++ test/support/qualified_typenames_example.ex
@@ -1,4 +1,9 @@
 defmodule QualifiedNamesExample do
+  # Ensures this module is only compiled after TypeCheck's recompilation.
+  # This is (only) necessary because this module is in the same project as TypeCheck's source itself.
+  # Without this, using type overrides such as `String.t()` would not work.
+  require TypeCheck.DefaultOverrides
+
   defstruct [:name]
   use TypeCheck, enable_runtime_checks: Mix.env() != :prod
   @type! t :: %__MODULE__{name: String.t()}
