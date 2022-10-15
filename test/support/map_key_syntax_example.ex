b3fecf884c6423678a5a83ac536eb166f423ae9c
Marten/Qqwy <w-m@resilia.nl>
2022-10-15 18:41:03 +0200

    Fixed the example. Turned out to be related to recompilation.
    (overrides are not available before recompilation.)

--- test/support/map_key_syntax_example.ex
+++ test/support/map_key_syntax_example.ex
@@ -1,10 +1,15 @@
 defmodule MapKeySyntaxExample do
+  # Ensures this module is only compiled after TypeCheck's recompilation.
+  # This is (only) necessary because this module is in the same project as TypeCheck's source itself.
+  # Without this, using type overrides such as `String.t()` would not work.
+  require TypeCheck.DefaultOverrides
+
   defstruct [:name]
   use TypeCheck
-  @type! t :: %__MODULE__{name: binary()}
+  @type! t :: %__MODULE__{name: String.t()}
 
   # binary() should be supported as map key
   # and be considered the same as `required(binary())`
-  @spec! example(%{binary() => any} | t()) :: t()
+  @spec! example(%{String.t() => any} | t()) :: t()
   def example(%__MODULE__{} = struct), do: struct
 end
