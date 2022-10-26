8adc8b15feee20eb8d34925ae18498263baf1260
Marten/Qqwy <w-m@resilia.nl>
2022-10-26 17:06:19 +0200

    Fix broken tests after mix format.

--- lib/type_check.ex
+++ lib/type_check.ex
@@ -283,7 +283,7 @@ defmodule TypeCheck do
       {:ok, 42}
       iex> {:error, type_error} = TypeCheck.dynamic_conforms(20, fourty_two)
       iex> type_error.message
-      "At (for doctest at) lib/type_check.ex:260:
+      "At (for doctest at) lib/type_check.ex:281:
           `20` is not the same value as `42`."
   """
   @spec dynamic_conforms(value, TypeCheck.Type.t()) ::
@@ -341,7 +341,7 @@ defmodule TypeCheck do
       iex> TypeCheck.dynamic_conforms!(42, fourty_two)
       42
       iex> TypeCheck.dynamic_conforms!(20, fourty_two)
-      ** (TypeCheck.TypeError) At lib/type_check.ex:323:
+      ** (TypeCheck.TypeError) At lib/type_check.ex:349:
           `20` is not the same value as `42`.
   """
   @spec dynamic_conforms!(value, TypeCheck.Type.t()) :: value | no_return()
